b4a4ef0f95fbc96f2f322b51cad43d4d645f71fc
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2023-09-14 22:34:18 -0300

    Integrating: back-end with the front-end + AI

--- server/download.js
+++ server/download.js
@@ -1,23 +1,28 @@
 import ytdl from "ytdl-core"
 import fs from "fs"
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise((resolve, reject) => {
   const videoURL = "https://www.youtube.com/shorts/" + videoId
-  console.log("Realizando o download do vídeo:" + videoId)
+  console.log("Realizando o download do vídeo:", videoId)
 
   ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
     .on("info", (info) => {
       const seconds = info.formats[0].approxDurationMs / 1000
 
       if (seconds > 60) {
-        throw new Error("A duração desse vídeo é maior que 60 segundos.")
+        throw new Error("A duração desse vídeo é maior do que 60 segundos.")
       }
     })
     .on("end", () => {
-      console.log("Download do vídeo realizado com sucesso!")
+      console.log("Download do vídeo finalizado.")
+      resolve()
     })
     .on("error", (error) => {
-      console.log("Ocorreu um erro ao realizar o download do vídeo. Detalhes do erro: " + error)
+      console.log(
+        "Não foi possível fazer o download do vídeo. Detalhes do erro:",
+        error
+      )
+      reject()
     })
     .pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+})
